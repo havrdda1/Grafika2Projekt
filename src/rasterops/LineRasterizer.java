a4aaae8ce019514084364830dbc0a2fb30929f0a
havrdda1 <43078184+havrdda1@users.noreply.github.com>
2019-05-02 00:26:01 +0200

    cosmetic change

--- src/rasterops/LineRasterizer.java
+++ src/rasterops/LineRasterizer.java
@@ -7,12 +7,13 @@ public interface LineRasterizer<T> {
     /**
      * Rasterizes a line in normalized coordinates ([-1;1] square),
      * upper image left corner in [-1;1], lower left corner in [1;-1]
+     *
      * @param background image to "add" the line to
-     * @param x1 x-coordinate of the start-point, in [-1;1]
-     * @param y1 y-coordinate of the start-point, in [-1;1]
-     * @param x2 x-coordinate of the end-point, in [-1;1]
-     * @param y2 y-coordinate of the end-point, in [-1;1]
-     * @param value value of the line pixels
+     * @param x1         x-coordinate of the start-point, in [-1;1]
+     * @param y1         y-coordinate of the start-point, in [-1;1]
+     * @param x2         x-coordinate of the end-point, in [-1;1]
+     * @param y2         y-coordinate of the end-point, in [-1;1]
+     * @param value      value of the line pixels
      * @return new image with the line added on the background
      */
     @NotNull Image<T> rasterize(
